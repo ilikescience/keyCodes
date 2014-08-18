61ee4b5bee508dde1cf5f4cc25d6bd5ccd0aa57d
Matt Strom <matthew.h.strom@gmail.com>
2014-08-18 19:37:23 -0400

    documented js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+// initialize keycode library
 var keycode = require('keycode');
 
 $(document).ready( function(){
+
+  //grab the keydown even
   $(window).keydown( function(event){
 
-    console.log(window.getSelection());
+    //if the user hasn't selected something (you know, to copy and paste), preventDefault and show the keycode
     if(window.getSelection().type !== "Range" || document.getSelection().type !== "Range"){
+      event.preventDefault();
       $('.code').html(event.which);
       $('.label').html(keycode(event));
     }
